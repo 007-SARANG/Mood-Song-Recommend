addd07c85f011325a97b09d9eb89507294d68e56
007-SARANG <sarangarora571@gmail.com>
2025-06-17 18:38:07 +0530

    Create app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>🎵 Mood-Based Song Recommender 🎵</h1>
+      <p>Start by selecting your mood!</p>
+    </div>
+  )
+}
+
+export default App
